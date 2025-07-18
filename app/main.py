157d5b5cbcb20055c195cce0a92b8ba019c66b25
Sezim <2017sezim17@gmail.com>
2025-07-18 07:38:34 +0500

    cors

--- app/main.py
+++ app/main.py
@@ -13,8 +13,9 @@ app = FastAPI(
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://githack.vercel.app/anketa"
+    "https://githack.vercel.app"
 ]
+
 
 
 app.add_middleware(
